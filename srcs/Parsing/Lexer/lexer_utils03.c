0114794b1497a53cdb3e1c6607a00c1c8277d1cb
Elbarae El-ghannouchi <barae.gh@gmail.com>
2022-05-24 17:22:28 +0100

    update files

---
+++ srcs/Parsing/Lexer/lexer_utils03.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_utils03.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/21 18:12:47 by eel-ghan          #+#    #+#             */
+/*   Updated: 2022/05/21 18:24:34 by eel-ghan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../Includes/header.h"
+
+t_token	*lexer_handle_dollar(t_lexer *lexer)
+{
+	char	c;
+
+	if (lexer_advance(lexer))
+	{
+		c = lexer->c;
+		if (lexer->c =='"' || lexer->c == '\'')
+			return (init_token(TOKEN_WORD, lexer_get_value_in_quote(lexer, c)));
+		// if ()
+	}
+	return (NULL);
+}
---
+++ srcs/Parsing/Lexer/redirection_utils.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirection_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/22 17:27:52 by eel-ghan          #+#    #+#             */
+/*   Updated: 2022/05/23 13:34:38 by eel-ghan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../../Includes/header.h"
+
+t_token	*lexer_collect_out_red(t_lexer *lexer)
+{
+	t_token	*token;
+
+	if (!lexer_advance(lexer))
+		return (init_token(TOKEN_ERROR, ERROR));
+	if (lexer->c == '>')
+	{
+		token = handle_append_out_red(lexer);
+		if (token != NULL)
+			return (token);
+	}
+	else if (lexer->c == '<')
+		return (init_token(TOKEN_ERROR, ERROR));
+	else if (lexer->c == '"' || lexer->c == '\'')
+		return (handle_red_with_quote(lexer, lexer->c));
+	lexer_skip_whitespace(lexer);
+	if (!lexer->c)
+		return (init_token(TOKEN_ERROR, ERROR));
+	return (init_token(TOKEN_OUTPUT, lexer_get_value(lexer)));
+}
+
+t_token	*lexer_collect_in_red(t_lexer *lexer)
+{
+	t_token	*token;
+
+	if (!lexer_advance(lexer))
+		return (init_token(TOKEN_ERROR, ERROR));
+	if (lexer->c == '<')
+	{
+		token = handle_heredoc_in_red(lexer);
+		if (token != NULL)
+			return (token);
+	}
+	else if (lexer->c == '>')
+		return (init_token(TOKEN_ERROR, ERROR));
+	else if (lexer->c == '"' || lexer->c == '\'')
+		return (handle_red_with_quote(lexer, lexer->c));
+	lexer_skip_whitespace(lexer);
+	if (!lexer->c)
+		return (init_token(TOKEN_ERROR, ERROR));
+	return (init_token(TOKEN_INPUT, lexer_get_value(lexer)));
+}
+
+t_token	*handle_append_out_red(t_lexer *lexer)
+{
+	t_token *token;
+
+	if (lexer_advance(lexer))
+	{
+		if (lexer->c == '>' || lexer->c == '<')
+		{
+			// free(token);
+			// free(lexer);
+			return (init_token(TOKEN_ERROR, ERROR));
+		}
+		else
+		{
+			lexer_skip_whitespace(lexer);
+			if (!lexer->c || lexer->c == '|')
+				return (init_token(TOKEN_ERROR, ERROR));
+			token = lexer_get_next_token(lexer);
+			return (init_token(TOKEN_APPEND, token->value));
+		}
+	}
+	if (lexer->c == ' ')
+	{
+		lexer_skip_whitespace(lexer);
+		return (init_token(TOKEN_OUTPUT, lexer_get_value(lexer)));
+	}
+	return (NULL);
+}
+
+t_token	*handle_heredoc_in_red(t_lexer *lexer)
+{
+	t_token	*token;
+
+	if (lexer_advance(lexer))
+	{
+		if (lexer->c == '>' || lexer->c == '<')
+		{
+			// free(token);
+			// free(lexer);
+			return (init_token(TOKEN_ERROR, ERROR));
+		}
+		else
+		{
+			lexer_skip_whitespace(lexer);
+			if (!lexer->c || lexer->c == '|')
+				return (init_token(TOKEN_ERROR, ERROR));
+			token = lexer_get_next_token(lexer);
+			return (init_token(TOKEN_HERE_DOC, token->value));
+		}
+	}
+	if (lexer->c == ' ')
+	{
+		lexer_skip_whitespace(lexer);
+		return (init_token(TOKEN_INPUT, lexer_get_value(lexer)));
+	}
+	return (NULL);;
+}
+
+t_token	*handle_red_with_quote(t_lexer *lexer, char c)
+{
+	char	*value;
+
+	value = lexer_get_value_in_quote(lexer, c);
+	if (lexer->c == c)
+		lexer_skip_quote(lexer);
+	return (init_token(TOKEN_OUTPUT, ft_strjoin(value, lexer_get_value(lexer))));
+}
+
