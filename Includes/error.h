7c43f00f615f985b305dbc4df1e6e246d3d927fa
Elbarae El-ghannouchi <barae.gh@gmail.com>
2022-05-30 18:37:05 +0100

    handle error

---
+++ Includes/error.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   error.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eel-ghan <eel-ghan@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/28 15:08:21 by eel-ghan          #+#    #+#             */
+/*   Updated: 2022/05/28 15:12:33 by eel-ghan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ERROR_H
+# define ERROR_H
+
+# define SYNTAX_ERROR "syntax error"
+# define PIPE_ERROR "pipe syntax error"
+# define REDIRECTION_ERROR "redirection syntax error"
+
+#endif
